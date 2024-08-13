f90b0d0d1f453bc67da2d1dacd1ea77e953a2914
Mitanshu Agrawal <mishu10005agrawal@gmail.com>
2024-08-13 23:37:26 +0530

    Refactor OpenCV app to use Flask and camera initialization

--- opencv/app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask,render_template,redirect
-import cv2
-
-app=Flask(__name__)
-camera=cv2.VideoCapture(0)  # 0 is the camera index  number
-
-@app.route('/')
-def index():
-    return render_template('index.html')
