5d22426bb5657aeaf1206862e9be026e78283553
Mitanshu Agrawal <mishu10005agrawal@gmail.com>
2024-08-13 22:05:35 +0530

    chore: Remove unused code and templatespen cv

--- opencv/app.py
+++ opencv/app.py
@@ -2,4 +2,8 @@ from flask import Flask,render_template,redirect
 import cv2
 
 app=Flask(__name__)
-camera=cv2.VideoCapture(0)  # 0 is the camera index  n   ed
+camera=cv2.VideoCapture(0)  # 0 is the camera index  number
+
+@app.route('/')
+def index():
+    return render_template('index.html')
